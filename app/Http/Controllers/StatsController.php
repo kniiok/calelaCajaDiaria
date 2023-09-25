14b13e71c9cf57ffcbce0f935d72d3e41cef69e8
kniiok <tomascaniza6@gmail.com>
2023-09-25 11:17:18 -0300

    grafico andando de diez

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -16,13 +16,62 @@ class StatsController extends Controller
                 DB::raw('YEAR(fecha) as anio'),
                 DB::raw('MONTH(fecha) as mes'),
                 'tipoproductos.tipo as tipo_producto',
-                DB::raw('SUM(montoEfectivo + montoTarjeta + montoTransferencia) as total_ventas')
+                DB::raw('SUM(montoEfectivo) as ventas_efectivo'),
+                DB::raw('SUM(montoTarjeta) as ventas_tarjeta'),
+                DB::raw('SUM(montoTransferencia) as ventas_transferencia'),
+                DB::raw('SUM(montoEfectivo + montoTarjeta + montoTransferencia) as total_ventas'),
+                DB::raw('COUNT(*) as cantidad_ventas')
             )
             ->groupBy('anio', 'mes', 'tipo_producto')
             ->orderBy('anio', 'desc')
             ->orderBy('mes', 'desc')
             ->get();
 
-            return view('stats.index', compact('statsData'));
-        }
+        $monthlySales = DB::table('ventas')
+            ->select(
+                DB::raw('DATE_FORMAT(fecha, "%Y-%m") as month'),
+                DB::raw('SUM(montoEfectivo + montoTarjeta + montoTransferencia) as total_sales')
+            )
+            ->groupBy('month')
+            ->orderBy('month')
+            ->get();
+            
+            // Consulta para obtener estadísticas de ventas por mes y tipo de producto (arreglos)
+            $arregloSales = DB::table('ventas')
+            ->join('tipoproductos', 'ventas.idTipoProducto', '=', 'tipoproductos.id')
+            ->select(
+                DB::raw('DATE_FORMAT(fecha, "%Y-%m") as month'),
+                DB::raw('SUM(montoEfectivo + montoTarjeta + montoTransferencia) as total_sales')
+            )
+            ->where('tipoproductos.tipo', '=', 'Arreglo') // Cambia 'Arreglos' por el nombre correcto
+            ->groupBy('month')
+            ->orderBy('month')
+            ->get();
+
+           // Consulta para obtener estadísticas de ventas por mes y tipo de producto (tela)
+           $telaSales = DB::table('ventas')
+           ->join('tipoproductos', 'ventas.idTipoProducto', '=', 'tipoproductos.id')
+           ->select(
+               DB::raw('DATE_FORMAT(fecha, "%Y-%m") as month'),
+               DB::raw('SUM(montoEfectivo + montoTarjeta + montoTransferencia) as total_sales')
+           )
+           ->where('tipoproductos.tipo', '=', 'Tela') // Cambia 'Arreglos' por el nombre correcto
+           ->groupBy('month')
+           ->orderBy('month')
+           ->get();
+
+           // Consulta para obtener estadísticas de ventas por mes y tipo de producto (merceria)
+           $merceriaSales = DB::table('ventas')
+           ->join('tipoproductos', 'ventas.idTipoProducto', '=', 'tipoproductos.id')
+           ->select(
+               DB::raw('DATE_FORMAT(fecha, "%Y-%m") as month'),
+               DB::raw('SUM(montoEfectivo + montoTarjeta + montoTransferencia) as total_sales')
+           )
+           ->where('tipoproductos.tipo', '=', 'Merceria') // Cambia 'Arreglos' por el nombre correcto
+           ->groupBy('month')
+           ->orderBy('month')
+           ->get();
+            
+        return view('stats.index', compact('statsData', 'monthlySales','arregloSales','telaSales', 'merceriaSales'));
+    }
 }
--- resources/views/stats/index.blade.php
+++ resources/views/stats/index.blade.php
@@ -9,22 +9,103 @@
                         <th class="py-2 px-4 border-b border-gray-200 text-left">Año</th>
                         <th class="py-2 px-4 border-b border-gray-200 text-left">Mes</th>
                         <th class="py-2 px-4 border-b border-gray-200 text-left">Tipo de Producto</th>
+                        <th class="py-2 px-4 border-b border-gray-200 text-left">Ventas en Efectivo</th>
+                        <th class="py-2 px-4 border-b border-gray-200 text-left">Ventas en Tarjeta</th>
+                        <th class="py-2 px-4 border-b border-gray-200 text-left">Ventas en Transferencia</th>
                         <th class="py-2 px-4 border-b border-gray-200 text-left">Total de Ventas</th>
+                        <th class="py-2 px-4 border-b border-gray-200 text-left">Cantidad de Ventas</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach($statsData as $stat)
                         <tr>
                             <td class="py-2 px-4 border-b border-gray-200 text-left">{{ $stat->anio }}</td>
-                            <td class="py-2 px-4 border-b border-gray-200 text-left">{{ date('F', mktime(0, 0, 0, $stat->mes, 10)) }}</td>
+                            <td class="py-2 px-4 border-b border-gray-200 text-left">{{ strftime('%B', mktime(0, 0, 0, $stat->mes, 10)) }}</td>
                             <td class="py-2 px-4 border-b border-gray-200 text-left">{{ $stat->tipo_producto }}</td>
+                            <td class="py-2 px-4 border-b border-gray-200 text-left">{{ $stat->ventas_efectivo }}</td>
+                            <td class="py-2 px-4 border-b border-gray-200 text-left">{{ $stat->ventas_tarjeta }}</td>
+                            <td class="py-2 px-4 border-b border-gray-200 text-left">{{ $stat->ventas_transferencia }}</td>
                             <td class="py-2 px-4 border-b border-gray-200 text-left">{{ $stat->total_ventas }}</td>
+                            <td class="py-2 px-4 border-b border-gray-200 text-left">{{ $stat->cantidad_ventas }}</td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
             <div>
-                <canvas id="myChart"></canvas>
+                <canvas id="monthlySalesChart" width="400" height="200"></canvas>
+            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+            <script>
+                var ctx = document.getElementById('monthlySalesChart').getContext('2d');
+                var labels = @json($monthlySales->pluck('month'));
+                var data = @json($monthlySales->pluck('total_sales'));
+                var dataArreglos = @json($arregloSales);
+                var dataTelas = @json($telaSales);
+                var dataMerceria = @json($merceriaSales);
+                var chart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: labels,
+                        datasets: [{
+                            label: 'Ventas por Mes',
+                            data: data,
+                            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                            borderColor: 'rgba(75, 192, 192, 1)',
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            x: [{
+                                type: 'time',
+                                time: {
+                                    unit: 'month',
+                                    displayFormats: {
+                                        month: 'MMM YYYY'
+                                    }
+                                },
+                                title: {
+                                    display: true,
+                                    text: 'Mes'
+                                }
+                            }],
+                            y: {
+                                title: {
+                                    display: true,
+                                    text: 'Ventas'
+                                }
+                            }
+                        }
+                    }
+                });
+                chart.data.datasets.push({
+        label: 'Ventas de Arreglos por Mes',
+        data: dataArreglos.map(entry => entry.total_sales),
+        backgroundColor: 'rgba(255, 99, 132, 0.2)', // Cambia el color si lo deseas
+        borderColor: 'rgba(255, 99, 132, 1)', // Cambia el color si lo deseas
+        borderWidth: 1,
+    });
+
+    chart.data.datasets.push({
+        label: 'Ventas de Telas por Mes',
+        data: dataTelas.map(entry => entry.total_sales),
+        backgroundColor: 'rgba(54, 162, 235, 0.2)', // Cambia el color si lo deseas
+        borderColor: 'rgba(54, 162, 235, 1)', // Cambia el color si lo deseas
+        borderWidth: 1,
+    });
+
+    chart.data.datasets.push({
+        label: 'Ventas de Mercería por Mes',
+        data: dataMerceria.map(entry => entry.total_sales),
+        backgroundColor: 'rgba(255, 206, 86, 0.2)', // Cambia el color si lo deseas
+        borderColor: 'rgba(255, 206, 86, 1)', // Cambia el color si lo deseas
+        borderWidth: 1,
+    });
+
+    // Actualiza el gráfico
+    chart.update();
+            </script>
+            
+            
             </div>
         </div>
     </div>
